b9e2c2771af7984815d21447f111d4499ad4c01d
Maverick4714 <144062999+Maverick4714@users.noreply.github.com>
2024-05-09 14:26:17 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-basic.showIcon(IconNames.Heart)
+input.onButtonPressed(Button.A, function () {
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinLeft, 101)
+})
+input.onButtonPressed(Button.B, function () {
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 101)
+})
+let distance = 0
 Tinybit.RGB_Car_Big(Tinybit.enColor.OFF)
 Tinybit.RGB_Car_Program().clear()
 Tinybit.RGB_Car_Program().show()
@@ -16,14 +22,14 @@ basic.forever(function () {
     Tinybit.RGB_Car_Program().showRainbow(1, 50)
 })
 basic.forever(function () {
-    if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 101)
-    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinLeft, 154)
-    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 154)
+    distance = Tinybit.Ultrasonic_Car()
+    if (distance < 15) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Back, 97)
+        basic.pause(200)
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 101)
+        basic.pause(200)
     } else {
-    	
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 255)
     }
 })
 basic.forever(function () {
