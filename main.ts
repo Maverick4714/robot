7b311c28ca5435e84ddca59862c78e92b99d790f
Maverick4714 <144062999+Maverick4714@users.noreply.github.com>
2024-05-10 14:42:45 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,19 +8,22 @@ let distance = 0
 Tinybit.RGB_Car_Big(Tinybit.enColor.OFF)
 Tinybit.RGB_Car_Program().clear()
 Tinybit.RGB_Car_Program().show()
-basic.forever(function () {
-    Tinybit.RGB_Car_Program().showRainbow(1, 50)
-})
-basic.forever(function () {
+loops.everyInterval(500, function () {
     distance = Tinybit.Ultrasonic_Car()
     if (distance < 5) {
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Back, 97)
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Back, 125)
         basic.pause(200)
         Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 173)
         basic.pause(200)
     } else {
         Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 255)
     }
+})
+basic.forever(function () {
+    Tinybit.RGB_Car_Program().showRainbow(1, 50)
+})
+basic.forever(function () {
+	
 })
 basic.forever(function () {
 	
